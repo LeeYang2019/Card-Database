f226f0ce5b3ffaf42ba710869d63f686997a9dd2
Nhialee Yang <nyang@madisoncollege.edu>
2019-09-19 14:41:43 -0500

    Add Card

---
+++ src/main/java/entity/Card.java
@@ -0,0 +1,42 @@
+
+public Class Card {
+
+  String cardName;
+  String cardType;
+  String cardEdition;
+  String cardSet;
+
+  public void setCardName(String cardName) {
+    this.cardName = cardName;
+  }
+
+  public void setCardType(String cardType) {
+    this.cardType = cardType;
+  }
+
+  public void setCardEdition(String cardEdition) {
+    this.cardEdition = cardEdition;
+  }
+
+  public void setCardSet(String cardSet) {
+    this.cardSet = cardSet;
+  }
+
+  public String getCardName() {
+    return this.cardName;
+  }
+
+  public String getCardType() {
+    return this.cardType;
+  }
+
+  public String getCardEdition() {
+    return this.cardEdition;
+  }
+
+  public String getCardSet() {
+    return this.cardSet;
+  }
+
+
+}
