dd4516ff677546cf5242f4e30134adb310fb78f4
Nhialee Yang <nyang@madisoncollege.edu>
2019-10-14 15:12:25 -0500

    Updates

--- src/test/java/persistence/CardDaoTest.java
+++ src/test/java/persistence/CardDaoTest.java
@@ -33,41 +33,57 @@ class CardDaoTest {
     @Test
     void getByCardName() {
         YugiohCard newCard = new YugiohCard();
-        YugiohCard secondCard = new YugiohCard();
         newCard = yugiohDao.getByCardName("Dark Magician");
-        secondCard = yugiohDao.getByCardId(1);
-        assertEquals(newCard.getCardName(), secondCard.getCardName());
+        assertEquals(newCard.getCardType(), "Monster");
     }
 
     @Test
     void getByCardType() {
+        YugiohCard newCard = new YugiohCard();
+        //newCard = yugiohDao.get
     }
 
     @Test
     void getByCardRarity() {
+        YugiohCard newCard = new YugiohCard();
     }
 
     @Test
     void getByCardSet() {
+        YugiohCard newCard = new YugiohCard();
     }
 
     @Test
     void getByCardPrice() {
+        YugiohCard newCard = new YugiohCard();
     }
 
     @Test
     void saveOrUpdate() {
+        YugiohCard newCard = new YugiohCard();
     }
 
     @Test
     void insert() {
+        YugiohCard newCard = new YugiohCard();
+        newCard.setCardName("Buster Blader");
+        newCard.setCardRarity("Ultra");
+        newCard.setCardPrice(30);
+        newCard.setCardType("Monster");
+        newCard.setCardSet("LON-035");
+
+        yugiohDao.insert(newCard);
+        yugiohDao.getAll().size();
+        assertEquals(3, yugiohDao.getAll().size());
     }
 
     @Test
     void delete() {
+        YugiohCard newCard = new YugiohCard();
     }
 
     @Test
     void getAll() {
+        assertEquals(3, yugiohDao.getAll().size());
     }
 }
