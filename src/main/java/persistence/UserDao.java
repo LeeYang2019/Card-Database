6aa19de3090f2305070b159b6d04193ed0b8b80e
Nhialee Yang <nyang@madisoncollege.edu>
2019-10-14 22:51:47 -0500

    project updates

--- src/main/java/persistence/UserDao.java
+++ src/main/java/persistence/UserDao.java
@@ -1,13 +1,17 @@
 package persistence;
 
+import entity.YugiohCard;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.hibernate.Transaction;
 
 public class UserDao {
+
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
 
 
 }
