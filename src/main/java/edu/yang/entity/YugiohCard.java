dd19788dccc8cc51b2d2495b5337f0cd19c796eb
Nhialee Yang <nyang@madisoncollege.edu>
2019-10-26 05:18:28 -0500

    updated yugiohCard class

--- src/main/java/edu/yang/entity/YugiohCard.java
+++ src/main/java/edu/yang/entity/YugiohCard.java
@@ -45,10 +45,23 @@ public class YugiohCard {
     @OneToMany (mappedBy = "yugiohCard", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private Set<YugiohCardHistory> entries = new HashSet<>();
 
+    /**
+     * no arg constructor
+     */
     public YugiohCard() {
 
     }
 
+    /**
+     * arg constructor
+     * @param cardName
+     * @param cardType
+     * @param cardRarity
+     * @param cardSet
+     * @param price
+     * @param quantity
+     * @param user
+     */
     public YugiohCard(String cardName, String cardType, String cardRarity, String cardSet, double price, int quantity, User user) {
         this.cardName = cardName;
         this.cardType = cardType;
@@ -59,29 +72,153 @@ public class YugiohCard {
         this.user = user;
     }
 
+    /**
+     * sets cardName
+     * @param cardName
+     */
     public void setCardName(String cardName) { this.cardName = cardName; }
+
+    /**
+     * sets cardType
+     * @param cardType
+     */
     public void setCardType(String cardType) { this.cardType = cardType; }
-    public String getCardName() { return this.cardName; }
-    public String getCardType() { return this.cardType; }
+
+    /**
+     * sets cardRarity
+     * @param cardRarity
+     */
+    public void setCardRarity(String cardRarity) {
+        this.cardRarity = cardRarity;
+    }
+
+    /**
+     * sets cardSet
+     * @param cardSet
+     */
+    public void setCardSet(String cardSet) {
+        this.cardSet = cardSet;
+    }
+
+    /**
+     * sets price
+     * @param price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * sets quantity
+     * @param quantity
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * sets user
+     * @param user
+     */
     public void setUser(User user) {
         this.user = user;
     }
+
+    /**
+     * gets cardName
+     * @return cardName
+     */
+    public String getCardName() { return this.cardName; }
+
+    /**
+     * gets cardType
+     * @return cardType
+     */
+    public String getCardType() { return this.cardType; }
+
+    /**
+     * gets cardRarity
+     * @return cardRarity
+     */
+    public String getCardRarity() {
+        return this.cardRarity;
+    }
+
+    /**
+     * gets cardSet
+     * @return cardSet
+     */
+    public String getCardSet() {
+        return this.cardSet;
+    }
+
+    /**
+     * gets price
+     * @return price
+     */
+    public double getPrice() {
+        return this.price;
+    }
+
+    /**
+     * gets quantity
+     * @return quantity
+     */
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    /**
+     * gets user
+     * @return user
+     */
     public User getUser() {
         return this.user;
     }
+
+    /**
+     * sets entries set
+     * @param entries
+     */
     public void setEntries(Set<YugiohCardHistory> entries) {
         this.entries = entries;
     }
+
+    /**
+     * gets entries
+     * @return entries
+     */
     public Set<YugiohCardHistory> getEntries() {
         return this.entries;
     }
+
+    /**
+     * sets an entry
+     * @param entry
+     */
     public void addEntry(YugiohCardHistory entry) {
         entries.add(entry);
         entry.setYugiohCard(this);
     }
+
+    /**
+     * removes an entry
+     * @param entry
+     */
     public void removeEntry(YugiohCardHistory entry) {
         entries.remove(entry);
         entry.setYugiohCard(null);
+    }
+
+    @Override
+    public String toString() {
+        return "user: " + this.user + "\n" +
+                "cardName : " + this.cardName + "\n" +
+                "cardType : " + this.cardType + "\n" +
+                "cardRarity : " + this.cardRarity + "\n" +
+                "cardSet : " + this.cardSet + "\n" +
+                "price : " + this.price + "\n" +
+                "quantity : " + this.quantity;
     }
 
 }
