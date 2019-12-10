b4634e8c437bb914edb31e2d4a805a6e2d847d21
Nhialee Yang <nyang@madisoncollege.edu>
2019-12-10 16:17:33 -0600

    update API servica call

--- src/main/java/edu/yang/service/TcgPlayerAPI.java
+++ src/main/java/edu/yang/service/TcgPlayerAPI.java
@@ -11,7 +11,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class TcgPlayer {
+public class TcgPlayerAPI {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
@@ -142,6 +142,85 @@ public class TcgPlayer {
             //logger.info(response);
 
             return response;
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return "";
+    }
+
+    public String getMarketPrice(int productID) {
+        String autToken2 = "-6_80xFIHbdtKHidNcclVivwwgsGM_Z07eHy7eKJ36caJ7Jle5VQ45YEkMWVR9KFs7u12YY9ZtcbHamSiSM4gExVB3QYLtpWvy1aj88Kt9gm8j1wLek2EDj-pum_OmOstBcNDFoHohmPJHogQDV50FscRQTZ0sUOBQZj6chttHVH_sOrDR1hUd4___dov8BACfGH3raK-QDMUh7IdY9SaMC8I2YZnmOZOXMWL4wh1gBUOfa8NKrwColtjKapZlAqHBMMElaKWnGoVZ04lzxUxv3KI7QcziJhE8CYeYiZ9VoKBCY01uUJqjzzmK5oNT0Aafxpuw";
+        String bearerToken = "bearer " + autToken2;
+        String response;
+
+
+        BufferedReader reader = null;
+
+        try {
+
+            URL url = new URL("http://api.tcgplayer.com/v1.32.0/pricing/product/" + productID);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Authorization", bearerToken);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setDoOutput(false);
+            connection.setRequestMethod("GET");
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line = null;
+            StringWriter out = new StringWriter(connection.getContentLength() > 0 ? connection.getContentLength() : 2048);
+
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            response = out.toString();
+            return response;
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return "";
+    }
+
+    public String searchCard(String paramOne, String paramTwo, String ParamThree) {
+        String autToken2 = "-6_80xFIHbdtKHidNcclVivwwgsGM_Z07eHy7eKJ36caJ7Jle5VQ45YEkMWVR9KFs7u12YY9ZtcbHamSiSM4gExVB3QYLtpWvy1aj88Kt9gm8j1wLek2EDj-pum_OmOstBcNDFoHohmPJHogQDV50FscRQTZ0sUOBQZj6chttHVH_sOrDR1hUd4___dov8BACfGH3raK-QDMUh7IdY9SaMC8I2YZnmOZOXMWL4wh1gBUOfa8NKrwColtjKapZlAqHBMMElaKWnGoVZ04lzxUxv3KI7QcziJhE8CYeYiZ9VoKBCY01uUJqjzzmK5oNT0Aafxpuw";
+        String bearerToken = "bearer " + autToken2;
+        BufferedReader buffReader = null;
+
+        try {
+
+            URL url = new URL("http://api.tcgplayer.com/v1.32.0/catalog/categories/2/search");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Authorization", bearerToken);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setDoOutput(true);
+            connection.setRequestMethod("Post");
+/*
+            String jsonInputString = `{"name": "ProductName", "Values": ["${paramOne}"]},` +
+                                     `{'name': 'SetName', 'Values': ['The Legend of Blue Eyes White Dragon']},` +
+                                     `{'name': 'Rarity', 'Values': ['Ultra']}`;
+*/
+            OutputStream os = connection.getOutputStream();
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+
+            buffReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream(), "utf-8"));
+
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = buffReader.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+
+            return response.toString();
 
         } catch (Exception e) {
             logger.error(e.getMessage());
--- src/test/java/edu/yang/service/TcgPlayerTest.java
+++ src/test/java/edu/yang/service/TcgPlayerTest.java
@@ -1,7 +1,6 @@
 package edu.yang.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
@@ -9,18 +8,10 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.json.Json;
-import javax.json.stream.JsonParser;
-
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class TcgPlayerTest {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
-    TcgPlayer tcgPlayerApi;
+    TcgPlayerAPI tcgPlayerApi;
     ObjectMapper objMapper;
 
     /**
@@ -28,7 +19,7 @@ class TcgPlayerTest {
      */
     @BeforeEach
     void setUp() {
-       tcgPlayerApi = new TcgPlayer();
+       tcgPlayerApi = new TcgPlayerAPI();
        objMapper = new ObjectMapper();
     }
 
@@ -39,8 +30,8 @@ class TcgPlayerTest {
 
     @Test
     void useBearerToken() {
-        TcgPlayer newTcgPlayer = new TcgPlayer();
-        logger.info(newTcgPlayer.useBearerToken());
+        TcgPlayerAPI newTcgPlayerAPI = new TcgPlayerAPI();
+        logger.info(newTcgPlayerAPI.useBearerToken());
     }
 
     @Test
@@ -51,11 +42,22 @@ class TcgPlayerTest {
             JsonNode resultsNode = jsonNode.get("results");
             logger.info("results path: " + resultsNode.toPrettyString());
 
-            //String imageUrl = resultsNode.get("imageUrl")
-
         } catch (JsonProcessingException e) {
             logger.error(e);
         }
 
+    }
+
+    @Test
+    void getProductPrice() {
+
+        try {
+            JsonNode jsonNode = objMapper.readTree(tcgPlayerApi.getMarketPrice(21876));
+            JsonNode resultsNode = jsonNode.get("results");
+            logger.info("results path: " + resultsNode.toPrettyString());
+
+        } catch (JsonProcessingException e) {
+            logger.error(e);
+        }
     }
 }
